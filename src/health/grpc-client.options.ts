40ad3d2c994cdfe46c07de9b396234e21363ad5c
Jeff Tian <jie.tian@unicareer.com>
2019-12-10 10:54:08 +0800

    fix: 127.0.0.1

--- src/health/grpc-client.options.ts
+++ src/health/grpc-client.options.ts
@@ -4,7 +4,7 @@ import {join} from 'path';
 export const grpcClientOptions: ClientOptions = {
     transport: Transport.GRPC,
     options: {
-        url: process.env.BIND_ADDRESS || '0.0.0.0:8080',
+        url: process.env.BIND_ADDRESS || '127.0.0.1:8080',
         package: 'grpc.health.v1',
         protoPath: join(__dirname, './health.proto')
     }
